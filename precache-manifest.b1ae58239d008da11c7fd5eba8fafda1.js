b2bf91ca24ae0db47d0da7a27abef48c369be1da
Siddhartha Singh 3 <sidsingh0@publicisgroupe.net>
2019-09-08 18:15:03 +0530

    Updates

---
+++ precache-manifest.b1ae58239d008da11c7fd5eba8fafda1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b47fe208bd768c23eefdc85bd2c989",
+    "url": "/echo-chatbot-deploy/index.html"
+  },
+  {
+    "revision": "b79b1228bedfec9d6991",
+    "url": "/echo-chatbot-deploy/static/css/main.23eafd0e.chunk.css"
+  },
+  {
+    "revision": "dc77061e192bf7a31ccf",
+    "url": "/echo-chatbot-deploy/static/js/2.d512f730.chunk.js"
+  },
+  {
+    "revision": "b79b1228bedfec9d6991",
+    "url": "/echo-chatbot-deploy/static/js/main.a18bed07.chunk.js"
+  },
+  {
+    "revision": "ed8dfb2c55e50216eb30",
+    "url": "/echo-chatbot-deploy/static/js/runtime~main.ee367b66.js"
+  }
+]);
